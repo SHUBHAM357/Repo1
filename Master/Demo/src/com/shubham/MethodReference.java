39185902f96a5ca3295e92d6aa7875a4c64b291c
irg <irg@SHUBHAM>
2020-10-04 23:43:24 +0530

    Initial commit

---
+++ Master/Demo/src/com/shubham/MethodReference.java
@@ -0,0 +1,12 @@
+package com.shubham;
+
+public class MethodReference {
+	
+	public static void information() {
+		System.out.println("some information is printing");
+	}
+
+	public static void information1() {
+		System.out.println("another information");
+	}
+}
---
+++ Master/Demo/src/com/shubham/Sayable.java
@@ -0,0 +1,6 @@
+package com.shubham;
+
+@FunctionalInterface
+public interface Sayable {
+ void say();
+}
